4abe70345bb9485ef518910280f388e1d9b53fe7
sonny <gyeomsony@gmail.com>
2024-12-15 19:03:50 +0900

    ✨[Fest] 포스터 이미지 뷰 추가

--- LuckVii/LuckVii/Controller/ViewController.swift
+++ LuckVii/LuckVii/Controller/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     
-    
+    // 포스터 이미지
+    private let posterImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.contentMode = .scaleAspectFit
+        imageview.backgroundColor = .lightGray
+        return imageview
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
