cd00ef42b8737360edad66ecdcfb179bf59d60a2
sonny <gyeomsony@gmail.com>
2024-12-15 19:06:43 +0900

    ✨[Feat] 영화 이름 레이블 뷰 추가

--- LuckVii/LuckVii/Controller/ViewController.swift
+++ LuckVii/LuckVii/Controller/ViewController.swift
@@ -16,6 +16,15 @@ class MovieDetailViewController: UIViewController {
         imageview.backgroundColor = .lightGray
         return imageview
     }()
+    
+    // 영화 이름 레이블
+    private let movieName: UILabel = {
+        let label = UILabel()
+        label.text = "영화 이름"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
