1d38e735da0d20f824b4a00c9cb7308cfb85d99a
jeff-0324 <jeffap324@gmail.com>
2024-12-17 21:24:32 +0900

    WIP

--- LuckVii/LuckVii/SceneDelegate.swift
+++ LuckVii/LuckVii/SceneDelegate.swift
@@ -9,7 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = UINavigationController(rootViewController: SignUpViewController())
         window.makeKeyAndVisible()
         
         self.window = window
