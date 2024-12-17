692b13978ac3b4ba9a67095809b32a95542c5801
jbnong07 <jbnong07@naver.com>
2024-12-17 11:02:33 +0900

    ✨ [Feat] MovieData
    define movie data struct

---
+++ LuckVii/LuckVii/Service/MovieData.swift
@@ -0,0 +1,21 @@
+//
+//  MovieData.swift
+//  LuckVii
+//
+//  Created by 박진홍 on 12/17/24.
+//
+
+import Foundation
+
+struct MovieData: Decodable {//인코딩 될 일이 없기 때문에 디코더블로 준수
+    let results: [Movie]
+}
+
+
+struct Movie: Decodable {
+    let adult: Bool//성인영화 여부
+    let originalLanguage: String//영화 원래 언어
+    let overview: String //설명
+    let popularity: Double//인기도
+    let posterPath: String?//포스터 > 없을 경우 대비 하여 옵셔널
+}
