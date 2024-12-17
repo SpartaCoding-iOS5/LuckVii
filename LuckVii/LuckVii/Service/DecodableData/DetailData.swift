32b167996de089a6305702377c9c4399c5105474
jbnong07 <jbnong07@naver.com>
2024-12-18 02:30:13 +0900

    ✨ [Feat] Get release date
    Add get release date method

--- LuckVii/LuckVii/Service/DecodableData/DetailData.swift
+++ LuckVii/LuckVii/Service/DecodableData/DetailData.swift
@@ -4,9 +4,22 @@
 //
 //  Created by 박진홍 on 12/18/24.
 //
+import Foundation
 
 struct DetailData: Decodable {
     let releaseDate: String
     let runtime: Int
     
+}
+
+extension DetailData {
+    func getReleaseDate() -> Date? {
+        let dateFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-mm-dd"
+            return formatter
+        }()
+        
+        return dateFormatter.date(from: releaseDate)
+    }
 }
