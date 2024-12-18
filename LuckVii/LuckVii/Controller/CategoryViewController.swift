deaefa8b3e9fc68f8424d544d4df03b06c8f629f
jbnong07 <jbnong07@naver.com>
2024-12-19 03:33:03 +0900

    ✨ [Feat] Add tab bar controller

---
+++ LuckVii/LuckVii/Controller/CategoryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryViewController.swift
+//  LuckVii
+//
+//  Created by 박진홍 on 12/19/24.
+//
+
+import UIKit
+
+final class CategoryViewController: UIViewController {
+    
+}
---
+++ LuckVii/LuckVii/Controller/MainTabBarController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  LuckVii
+//
+//  Created by 손겸 on 12/16/24.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    // MARK: - set up tab bar controller
+    
+    private func setupTabBar() {
+        let categoryVC: CategoryViewController = CategoryViewController()
+        let searchVC: SearchViewController = SearchViewController()
+        let myPageVC: MyPageViewController = MyPageViewController()
+        
+//        let categoryNav: UINavigationController = UINavigationController(rootViewController: categoryVC)
+//        let searchNav: UINavigationController = UINavigationController(rootViewController: searchVC)
+        let myPageNav: UINavigationController = UINavigationController(rootViewController: myPageVC)
+        let categoryNav: CategoryViewController = CategoryViewController()
+        let searchNav: SearchViewController = SearchViewController()
+//        let myPageNav: MyPageViewController = MyPageViewController()
+        //set tab bar item
+        categoryNav.tabBarItem = UITabBarItem(
+            title: "Category",
+            image: UIImage(systemName: "film"),
+            selectedImage: UIImage(systemName: "film.fill"))
+        searchNav.tabBarItem = UITabBarItem(
+            title: "Search",
+            image: UIImage(systemName: "magnifyingglass.circle"),
+            selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
+        myPageNav.tabBarItem = UITabBarItem(
+            title: "MyPage",
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill"))
+        
+        viewControllers = [categoryNav, searchNav, myPageNav]
+        
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .black
+    }
+
+}
