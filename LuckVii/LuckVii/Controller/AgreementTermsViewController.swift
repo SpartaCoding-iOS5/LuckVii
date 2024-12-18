e4895a6936906d6a7ea03602affd0aea13878114
name-mun <tains9153@gmail.com>
2024-12-18 13:40:47 +0900

    ✨ [Feat]: AgreementTermsViewController` 구현

---
+++ LuckVii/LuckVii/Controller/AgreementTermsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AgreementTermsViewController.swift
+//  LuckVii
+//
+//  Created by mun on 12/18/24.
+//
+
+import UIKit
+import SnapKit
+
+class AgreementTermsViewController: UIViewController {
+
+    let agreementTermsView = AgreementTermsView()
+
+    override func loadView() {
+        view = AgreementTermsView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
