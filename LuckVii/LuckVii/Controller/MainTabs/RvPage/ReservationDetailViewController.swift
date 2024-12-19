8d0cb0d2f0a222ece434d80d3c3a6311c30c6a15
jamong-mini <jamong.alsl@gmail.com>
2024-12-19 15:18:24 +0900

    ✨ [Feat]: 고객 예매 상세페이지 뷰 컨트롤러 구현
    - 더미 데이터 넣은 상태

---
+++ LuckVii/LuckVii/Controller/MainTabs/RvPage/ReservationDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ReservationDetailViewController.swift
+//  LuckVii
+//
+//  Created by Jamong on 12/19/24.
+//
+
+import UIKit
+import SnapKit
+
+class ReservationDetailViewController: UIViewController {
+   
+   // MARK: - Properties
+   private let detailView = ReservationDetailView()
+   
+   // MARK: - Initialization
+   init() {
+       super.init(nibName: nil, bundle: nil)
+   }
+   
+   required init?(coder: NSCoder) {
+       fatalError("init(coder:) has not been implemented")
+   }
+   
+   // MARK: - Lifecycle
+   override func loadView() {
+       view = detailView
+   }
+   
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       view.backgroundColor = .white
+       setupUI()
+       // 화면을 테스트하기 위해 임시 데이터를 configure로 전달
+       detailView.configure(
+           with: MovieReservation(
+               title: "테스트 영화",
+               dateTime: "2024.12.16 (월) 15:00~16:00 관람",
+               theater: "테스트 극장",
+               price: 0,
+               posterImage: nil as String?
+           )
+       )
+   }
+   
+   // MARK: - Setup
+   private func setupUI() {
+       navigationItem.title = "예매 상세 내역"
+   }
+}
